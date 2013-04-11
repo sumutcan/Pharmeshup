c411fa8cfcc79a706c5144433186f8e30633180c
Umutcan Simsek <s.umutcan@gmail.com>
2013-04-12 02:44:03 +0300

    - general info tab created

---
+++ src/main/gui/pnlGeneral.java
@@ -0,0 +1,116 @@
+package main.gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.TextArea;
+import javax.swing.JSeparator;
+import javax.swing.UIManager;
+import javax.swing.SwingConstants;
+import javax.swing.JEditorPane;
+import javax.swing.JList;
+
+public class pnlGeneral extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public pnlGeneral() {
+		setSize(new Dimension(770,760));
+		setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		add(splitPane);
+		
+		JSplitPane splitPane_1 = new JSplitPane();
+		splitPane_1.setDividerSize(5);
+		splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane.setLeftComponent(splitPane_1);
+		
+		JPanel panel = new JPanel();
+		splitPane_1.setLeftComponent(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel label = new JLabel("Description");
+		label.setForeground(UIManager.getColor("OptionPane.warningDialog.titlePane.foreground"));
+		label.setFont(new Font("Ubuntu Medium", Font.BOLD, 15));
+		panel.add(label, BorderLayout.CENTER);
+		
+		JPanel panel_1 = new JPanel();
+		splitPane_1.setRightComponent(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		
+		JTextPane txtPaneDBPedia = new JTextPane();
+		txtPaneDBPedia.setEditable(false);
+		panel_1.add(txtPaneDBPedia);
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		panel_1.add(separator);
+		
+		JTextPane textPaneDrugBank = new JTextPane();
+		textPaneDrugBank.setEditable(false);
+		panel_1.add(textPaneDrugBank);
+		splitPane_1.setDividerLocation(30);
+		
+		JSplitPane splitPane_2 = new JSplitPane();
+		splitPane_2.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane.setRightComponent(splitPane_2);
+		
+		JSplitPane splitPane_3 = new JSplitPane();
+		splitPane_3.setDividerSize(5);
+		splitPane_2.setLeftComponent(splitPane_3);
+		
+		JSplitPane splitPane_4 = new JSplitPane();
+		splitPane_4.setDividerSize(2);
+		splitPane_4.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane_3.setLeftComponent(splitPane_4);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setFont(new Font("Ubuntu Medium", Font.BOLD, 12));
+		splitPane_4.setLeftComponent(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblRelatedPages = new JLabel("Related Pages");
+		lblRelatedPages.setFont(new Font("Ubuntu", Font.BOLD, 13));
+		lblRelatedPages.setForeground(UIManager.getColor("OptionPane.warningDialog.titlePane.foreground"));
+		panel_2.add(lblRelatedPages, BorderLayout.WEST);
+		
+		JEditorPane paneRelatedPages = new JEditorPane();
+		paneRelatedPages.setEditable(false);
+		paneRelatedPages.setContentType("text/html");
+		paneRelatedPages.setText("<a href=\"google.com\">Google</a>");
+		splitPane_4.setRightComponent(paneRelatedPages);
+		splitPane_4.setDividerLocation(20);
+		
+		JSplitPane splitPane_5 = new JSplitPane();
+		splitPane_5.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane_5.setDividerSize(2);
+		splitPane_3.setRightComponent(splitPane_5);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setFont(new Font("Ubuntu Medium", Font.BOLD, 12));
+		splitPane_5.setLeftComponent(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblSynonyms = new JLabel("Synonyms");
+		lblSynonyms.setFont(new Font("Ubuntu", Font.BOLD, 13));
+		lblSynonyms.setForeground(UIManager.getColor("OptionPane.warningDialog.titlePane.foreground"));
+		panel_3.add(lblSynonyms, BorderLayout.WEST);
+		
+		JList list = new JList();
+		splitPane_5.setRightComponent(list);
+		splitPane_5.setDividerLocation(20);
+		splitPane_3.setDividerLocation(380);
+		splitPane_2.setDividerLocation(90);
+		splitPane.setDividerLocation(200);
+	}
+
+}

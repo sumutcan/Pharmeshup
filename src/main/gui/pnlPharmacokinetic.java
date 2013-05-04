0492ab290a71e9dec52d53ed7552a881c8d376ac
Umutcan Simsek <s.umutcan@gmail.com>
2013-05-04 16:21:26 +0300

    pnlPharmacokinetic

---
+++ src/main/gui/pnlPharmacokinetic.java
@@ -0,0 +1,18 @@
+package main.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class pnlPharmacokinetic extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public pnlPharmacokinetic() {
+		setSize(new Dimension(770,760));
+		setLayout(new BorderLayout(0, 0));
+	}
+
+}
